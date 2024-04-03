1fbfab3d899e67a58e2986557d8aa14c17c9c4cd
Michael Ullrich <michael.ullrich@nuvibit.com>
2024-04-03 21:20:59 +0200

    Refactor Test-Cases

--- examples/ssm-ps-1/outputs.tf
+++ examples/ssm-ps-1/outputs.tf
@@ -1,5 +1,9 @@
-
 output "test_success" {
   description = "Are local.configuration_add_on similar to the read configuration?"
   value       = local.configuration_add_on == module.core_configuration_reader.unflattened_configuration
+}
+
+output "core_configuration_reader" {
+  description = "Read configuration?"
+  value       = module.core_configuration_reader.unflattened_configuration
 }
