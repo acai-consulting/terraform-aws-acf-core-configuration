b0b122efa43b566ad8455bdae578ba8cf00c8c53
Michael Ullrich <michael.ullrich@acai.gmbh>
2025-04-23 11:19:23 +0200

    fix

--- examples/ssm-ps-4/outputs.tf
+++ examples/ssm-ps-4/outputs.tf
@@ -1,10 +1,7 @@
 output "test_success" {
   description = "Are local.configuration_add_on similar to the read configuration?"
-  value = merge(
-    local.configuration_add_on,
-    local.configuration_add_on1,
-    local.configuration_add_on2
-  ) == module.core_configuration_reader.unflattened_configuration
+  value       = local.configuration_add_on == module.core_configuration_reader.unflattened_configuration
+
 }
 
 output "core_configuration_reader" {
